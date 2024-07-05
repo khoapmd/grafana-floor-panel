88a00eeb093220448e0f975b6c7a5a8b9ebb1df5
khoapmd <khoa.pmd@outlook.com>
2024-07-05 14:43:32 +0700

    change plugin id

--- pack.sh
+++ pack.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-PLUGIN_ID="innorenew-iaq-panel"
+PLUGIN_ID="khoapmd-environment-panel"
 # pnpm run build
 rm -r $PLUGIN_ID
 cp -R dist $PLUGIN_ID
 zip -FSr "${PLUGIN_ID}.zip" $PLUGIN_ID
 md5sum "${PLUGIN_ID}.zip" > "${PLUGIN_ID}.md5"
-#unzip innorenew-iaq-panel
+#unzip khoapmd-environment-panel
